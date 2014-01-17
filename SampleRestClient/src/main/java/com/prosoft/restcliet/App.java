99f633e29979180aeab124c151195d5da6451a60
Sherla <Sherla@Sherla-PC>
2014-01-17 15:44:50 +0530

    first commit

---
+++ SampleRestClient/src/main/java/com/prosoft/restcliet/App.java
@@ -0,0 +1,101 @@
+package com.prosoft.restcliet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class App {
+	public static void main(String[] args) {
+		get();
+		post();
+	}
+
+	private static void get() {
+		DefaultHttpClient httpClient =null;
+		try {
+
+			httpClient = new DefaultHttpClient();
+			HttpGet getRequest = new HttpGet(
+					"http://localhost:8080/calc/sampledata");
+			getRequest.addHeader("accept", "application/json");
+
+			HttpResponse response = httpClient.execute(getRequest);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatusLine().getStatusCode());
+			}
+
+			System.out.println(response.getEntity().getContent());
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(response.getEntity().getContent())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+		} catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+
+	}
+	
+	private static void post() {
+		try {
+			 
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost(
+				"http://localhost:8080/calc/add");
+	 
+			String string = "{\"number1\":12,\"number2\":45}";
+			StringEntity input = new StringEntity(string);
+			input.setContentType("application/json");
+			postRequest.setEntity(input);
+	 
+			HttpResponse response = httpClient.execute(postRequest);
+	 
+			if (response.getStatusLine().getStatusCode() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatusLine().getStatusCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(
+	                        new InputStreamReader((response.getEntity().getContent())));
+	 
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+	 
+			httpClient.getConnectionManager().shutdown();
+	 
+		  } catch (MalformedURLException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (IOException e) {
+	 
+			e.printStackTrace();
+	 
+		  }
+	 
+	}
+}
